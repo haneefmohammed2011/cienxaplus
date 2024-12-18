b83d1231bc82c7c84a9155993ef4917c98ca7f93
haneefmohammed2011 <119083671+haneefmohammed2011@users.noreply.github.com>
2024-12-18 20:18:52 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// Simple horizontal carousel scroll
+document.querySelectorAll('.carousel').forEach(carousel => {
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  carousel.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX - carousel.offsetLeft;
+    scrollLeft = carousel.scrollLeft;
+  });
+
+  carousel.addEventListener('mouseleave', () => {
+    isDown = false;
+  });
+
+  carousel.addEventListener('mouseup', () => {
+    isDown = false;
+  });
+
+  carousel.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - carousel.offsetLeft;
+    const walk = (x - startX) * 2; // Scroll speed
+    carousel.scrollLeft = scrollLeft - walk;
+  });
+});
